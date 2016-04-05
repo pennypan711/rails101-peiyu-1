44f1d8108f79c3aa137bdd151b2012273484af1a
peiyupan <fishfish711@gmail.com>
2016-04-05 12:29:42 +0800

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'groups#index'
+  resources :groups do
+    member do
+      post :join
+      post :quit
+    end
 
+    resources :posts
+  end
+
+  namespace :account do
+    resources :groups
+    resources :posts
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
